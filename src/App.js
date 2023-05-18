65fa3d02516a26433eb19b11e4a03c5724b3ee45
Ceri Dorsey-Tyler <cerimay@gmail.com>
2023-05-18 18:45:53 -0400

    DONE

--- src/App.js
+++ src/App.js
@@ -13,52 +13,75 @@ import FindaCart from './FindaCart.png'
 import a500Logo from './a500Logo.png'
 
 function App() {
-  const [location, setLocation] = useState('')
-  const [foodTrucks, setFoodTrucks] = useState([])
+  const [location, setLocation] = useState('');
+  const [foodItems, setFoodItems] = useState('');
+  const [foodTrucks, setFoodTrucks] = useState([]);
 
   const handleLocationChange = (event) => {
-    setLocation(event.target.value)
-  }
+    setLocation(event.target.value);
+  };
+
+  const handleFoodItemsChange = (event) => {
+    setFoodItems(event.target.value);
+  };
 
   const handleSubmit = async (event) => {
-    event.preventDefault()
-
+    event.preventDefault();
+  
     try {
       const response = await axios.get(
         'https://data.sfgov.org/resource/rqzj-sfat.json'
-      )
-      const data = response.data
-
+      );
+      const data = response.data;
+  
       const filteredFoodTrucks = data.filter((truck) => {
-        const isLocationMatch = truck.address
-          .toLowerCase()
-          .includes(location.toLowerCase())
-        return isLocationMatch
-      })
+        const isLocationMatch =
+          truck.address &&
+          truck.address.toLowerCase().includes(location.toLowerCase());
+      
+        const isFoodItemsMatch =
+          truck.fooditems &&
+          truck.fooditems.toLowerCase().includes(foodItems.toLowerCase());
+      
+        const isFoodItemsArrayMatch =
+          Array.isArray(truck.fooditems) &&
+          truck.fooditems.some(
+            (item) => item.toLowerCase().includes(foodItems.toLowerCase())
+          );
+      
+        console.log('Location:', isLocationMatch);
+        console.log('Food Items:', isFoodItemsMatch || isFoodItemsArrayMatch);
+      
+        return isLocationMatch && (isFoodItemsMatch || isFoodItemsArrayMatch);
+      });
+  
+      console.log('Filtered Food Trucks:', filteredFoodTrucks); // Add this line to log the filtered food trucks
 
       const formattedFoodTrucks = filteredFoodTrucks.map((truck) => ({
         ...truck,
-        latitude: parseFloat(truck.location.latitude),
-        longitude: parseFloat(truck.location.longitude),
-      }))
+        latitude: parseFloat(truck.latitude),
+        longitude: parseFloat(truck.longitude),
+      }));
 
-      setFoodTrucks(formattedFoodTrucks)
+      setFoodTrucks(formattedFoodTrucks);
     } catch (error) {
-      console.error('Error fetching data:', error)
+      console.error('Error fetching data:', error);
     }
-  }
+  };
 
   return (
     <div
       className="min-h-screen bg-cover bg-center bg-no-repeat"
-      style={{ backgroundImage: `url(${bg})` }}>
+      style={{ backgroundImage: `url(${bg})` }}
+    >
       <div className="flex justify-center items-center h-screen">
         <div className="relative w-4/5 md:w-4/5 h-2/3 md:h-4/5 border-4 border-white rounded-3xl overflow-hidden">
           <MapContainer
             center={[37.7749, -122.4194]}
             zoom={13}
             className="h-full w-full"
-            style={{ position: 'absolute', zIndex: 0 }}>
+            style={{ position: 'absolute', zIndex: 0 }}
+          >
             {/* Your map content */}
             <TileLayer
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -74,7 +97,8 @@ function App() {
                   iconSize: [32, 32],
                   iconAnchor: [16, 32],
                   popupAnchor: [0, -32],
-                })}>
+                })}
+              >
                 <Popup>
                   <h3 className="text-lg font-bold">{truck.applicant}</h3>
                   <p className="text-sm">Cuisine: {truck.fooditems}</p>
@@ -85,10 +109,9 @@ function App() {
           </MapContainer>
           <div
             className="absolute top-4 right-4 bg-amber-50 p-4 max-w-xs md:max-w-sm w-11/12 md:w-2/3 lg:w-1/2 rounded-3xl"
-            style={{ zIndex: 1 }}>
-            <form
-              onSubmit={handleSubmit}
-              className="flex flex-col items-center h-full">
+            style={{ zIndex: 1 }}
+          >
+            <form onSubmit={handleSubmit} className="flex flex-col items-center h-full">
               <div className="flex flex-col items-center justify-center flex-grow">
                 <img
                   className="h-8 md:h-16 object-contain mb-4"
@@ -102,11 +125,20 @@ function App() {
                     value={location}
                     onChange={handleLocationChange}
                     className="p-3 border border-gray-300 rounded mr-2 w-full text-base md:text-lg"
-                    placeholder="Where ya at?"
+                    placeholder="Where at?"
+                  />
+                  <input
+                    type="text"
+                    id="foodItems"
+                    value={foodItems}
+                    onChange={handleFoodItemsChange}
+                    className="p-3 border border-gray-300 rounded mr-2 w-full text-base md:text-lg"
+                    placeholder="Food type?"
                   />
                   <button
                     type="submit"
-                    className="bg-slate-400 hover:bg-slate-500 text-white font-bold py-3 px-6 rounded text-base md:text-lg">
+                    className="bg-slate-400 hover:bg-slate-500 text-white font-bold py-3 px-6 rounded text-base md:text-lg"
+                  >
                     Search
                   </button>
                 </div>
@@ -116,7 +148,8 @@ function App() {
         </div>
       </div>
     </div>
-  )
+  );
 }
+
 
 export default App
