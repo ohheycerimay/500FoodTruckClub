971c261ea532d57b001bea2b9dc1971c15e0222c
Ceri Dorsey-Tyler <cerimay@gmail.com>
2023-05-18 14:12:51 -0400

    formatting

--- src/App.js
+++ src/App.js
@@ -49,11 +49,16 @@ function App() {
   }
 
   return (
-    <div className="min-h-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${bg})` }}>
+    <div
+      className="min-h-screen bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: `url(${bg})` }}>
       <div className="flex justify-center items-center h-screen">
         <div className="relative w-4/5 md:w-4/5 h-2/3 md:h-4/5 border-4 border-white rounded-3xl overflow-hidden">
-          <MapContainer center={[37.7749, -122.4194]} zoom={13} className="h-full w-full" style={{ position: 'absolute', zIndex: 0 }}>
-            {/* Your map content */}
+          <MapContainer
+            center={[37.7749, -122.4194]}
+            zoom={13}
+            className="h-full w-full"
+            style={{ position: 'absolute', zIndex: 0 }}>
             <TileLayer
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               attribution="Map data © OpenStreetMap contributors"
@@ -63,12 +68,12 @@ function App() {
                 key={truck.objectid}
                 position={[truck.latitude, truck.longitude]}
                 icon={L.icon({
-                  iconUrl: 'https://cdn-icons-png.flaticon.com/512/683/683071.png',
+                  iconUrl:
+                    'https://cdn-icons-png.flaticon.com/512/683/683071.png',
                   iconSize: [32, 32],
                   iconAnchor: [16, 32],
                   popupAnchor: [0, -32],
-                })}
-              >
+                })}>
                 <Popup>
                   <h3 className="text-lg font-bold">{truck.applicant}</h3>
                   <p className="text-sm">Cuisine: {truck.fooditems}</p>
@@ -77,10 +82,20 @@ function App() {
               </Marker>
             ))}
           </MapContainer>
-          <div className="absolute top-4 right-4 bg-amber-50 p-4 max-w-xs md:max-w-xsm w-11/12 md:w-1/3 lg:w-1/2 h-1/4 md:h-2/5 rounded-3xl" style={{ zIndex: 1 }}>
-            <form onSubmit={handleSubmit} className="flex flex-col items-center">
-              <label htmlFor="location" className="text-lg md:text-2xl text-slate-400 font-bold mb-4">
-                <img className="h-12 md:h-24" src={FindaCart} alt="Find a Cart" />
+          <div
+            className="absolute top-4 right-4 bg-amber-50 p-4 max-w-xs md:max-w-xsm w-11/12 md:w-1/3 lg:w-1/2 h-1/4 md:h-2/5 rounded-3xl"
+            style={{ zIndex: 1 }}>
+            <form
+              onSubmit={handleSubmit}
+              className="flex flex-col items-center">
+              <label
+                htmlFor="location"
+                className="text-lg md:text-2xl text-slate-400 font-bold mb-4">
+                <img
+                  className="h-12 md:h-24"
+                  src={FindaCart}
+                  alt="Find a Cart"
+                />
               </label>
               <div className="flex items-center w-full">
                 <input
@@ -93,8 +108,7 @@ function App() {
                 />
                 <button
                   type="submit"
-                  className="bg-slate-400 hover:bg-slate-500 text-white font-bold py-3 px-6 rounded ml-2 text-base md:text-lg"
-                >
+                  className="bg-slate-400 hover:bg-slate-500 text-white font-bold py-3 px-6 rounded ml-2 text-base md:text-lg">
                   Search
                 </button>
               </div>
@@ -102,21 +116,8 @@ function App() {
           </div>
         </div>
       </div>
-  </div>
-  );
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+    </div>
+  )
 }
 
 export default App
