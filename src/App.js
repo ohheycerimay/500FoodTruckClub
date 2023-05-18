88deba2c1c4490381cebae4dfc393a1ecce10993
Ceri Dorsey-Tyler <cerimay@gmail.com>
2023-05-18 02:19:24 -0400

    functionality

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'
 import 'leaflet-defaulticon-compatibility'
-import './index.css';
+import './index.css'
 import './styles.css'
 import 'typeface-smokum'
 import bg from './bg.webp'
@@ -48,65 +48,69 @@ function App() {
 
   return (
     <div className="min-h-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${bg})` }}>
-      <div className="flex justify-center">
-        <div className="container mx-auto py-8 px-4">
-        <div className="bg-slate-400 inline-block">
-  <h1 className="text-4xl text-amber-50 font-bold mb-6 font-smokum text-center py-4 px-8">
-    500 Food Trucks Club
-  </h1>
+  <div className="flex justify-center">
+    <div className="container mx-auto py-8 px-4">
+      <div className="bg-slate-400 inline-block rounded-lg">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl text-amber-50 font-bold font-smokum text-center py-4 px-8">
+          500 Food Trucks Club
+        </h1>
+      </div>
+      <div className="flex flex-col items-center bg-amber-50 mt-8 p-4 rounded-lg">
+  <form className="w-full md:w-auto" onSubmit={handleSubmit}>
+    <label htmlFor="location" className="text-lg md:text-2xl text-slate-400 font-bold mt-2 mb-4">
+      Find Truck Locations Near You:
+    </label>
+    <div className="flex items-center">
+      <input
+        type="text"
+        id="location"
+        value={location}
+        onChange={handleLocationChange}
+        className="p-3 border border-gray-300 rounded mr-2 w-full"
+        placeholder="Enter your location"
+      />
+      <button
+        type="submit"
+        className="bg-slate-400 hover:bg-slate-500 text-white font-bold py-3 px-6 rounded ml-2"
+      >
+        Search
+      </button>
+    </div>
+  </form>
 </div>
-          <div className=" flex justify-center bg-amber-50 inline-block">
-            <form className="flex justify-center mb-8 " onSubmit={handleSubmit}>
-              <label htmlFor="location" className="text-4xl text-slate-400 font-bold mr-2 mt-6">
-                Find Truck Locations Near You:
-              </label>
-              <input
-                type="text"
-                id="location"
-                value={location}
-                onChange={handleLocationChange}
-                className="p-2 border border-gray-300 rounded mt-6"
-              />
-              <button
-                type="submit"
-                className="bg-slate-400 hover:bg-slate-500 text-white font-bold py-2 px-4 rounded ml-2 mt-6"
-              >
-                Search
-              </button>
-            </form>
-          </div>
-          <div className="w-full h-96 py-2 px-4 rounded">
-            <MapContainer center={[37.7749, -122.4194]} zoom={13} className="h-full shadow-2xl">
-              <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution="Map data © OpenStreetMap contributors"
-              />
-              {foodTrucks.map((truck) => (
-                <Marker
-                  key={truck.objectid}
-                  position={[truck.latitude, truck.longitude]}
-                  icon={L.icon({
-                    iconUrl: 'https://cdn-icons-png.flaticon.com/512/683/683071.png',
-                    iconSize: [32, 32],
-                    iconAnchor: [16, 32],
-                    popupAnchor: [0, -32],
-                  })}
-                >
-                  <Popup>
-                    <h3>{truck.applicant}</h3>
-                    <p>Cuisine: {truck.fooditems}</p>
-                    <p>Address: {truck.address}</p>
-                  </Popup>
-                </Marker>
-              ))}
-            </MapContainer>
-          </div>
+
+  
+      <div className="w-full h-96 py-4 rounded">
+  <MapContainer center={[37.7749, -122.4194]} zoom={13} className="h-full shadow-lg rounded">
+    <TileLayer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      attribution="Map data © OpenStreetMap contributors"
+    />
+    {foodTrucks.map((truck) => (
+      <Marker
+        key={truck.objectid}
+        position={[truck.latitude, truck.longitude]}
+        icon={L.icon({
+          iconUrl: 'https://cdn-icons-png.flaticon.com/512/683/683071.png',
+          iconSize: [32, 32],
+          iconAnchor: [16, 32],
+          popupAnchor: [0, -32],
+        })}
+      >
+        <Popup>
+          <h3 className="text-lg font-bold">{truck.applicant}</h3>
+          <p className="text-sm">Cuisine: {truck.fooditems}</p>
+          <p className="text-sm">Address: {truck.address}</p>
+        </Popup>
+      </Marker>
+    ))}
+  </MapContainer>
+</div>
         </div>
       </div>
+      
     </div>
-  );
-  
-  
+  )
 }
 
 export default App
